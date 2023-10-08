d924fd620131f6169328c6c38c60c0b1b6fa8c78
Baconzilla123 <105701413+Baconzilla123@users.noreply.github.com>
2023-10-08 14:47:43 +1100

    I FORGOR

--- src/launch.lua
+++ src/launch.lua
@@ -1,7 +1,7 @@
 local context = require "artist"()
 
 -- Feel free to include custom modules here:
-if peripheral.find("monitor")[1] ~= nil then
+if peripheral.find("monitor") ~= nil then
     context:require "display"
 end
 
