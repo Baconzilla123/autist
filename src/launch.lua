7d79858212ce1ca7a774ab6853aab5c3ae1c49fa
Baconzilla123 <105701413+Baconzilla123@users.noreply.github.com>
2023-12-27 16:06:16 +1100

    improve updater

--- src/launch.lua
+++ src/launch.lua
@@ -1,6 +1,21 @@
 --function wait(seconds) local start = os.time() repeat until os.time() > start + seconds end
 
 print("Checking For Updates...")
+
+local req, err = http.get("https://raw.githubusercontent.com/baconzilla123/autist/HEAD/version.txt")
+if not req then 
+    error("Version Fetch failed: " .. err, 0) 
+else
+    local file = fs.open(".artist.d/version", "r")
+    local version = file.readLine()
+    
+    if version ~= req.readAll() then
+        print("Version "..version.." Does not match "..req:readAll())
+    end
+
+    --file.write(req.readAll())
+end
+
 shell.run("wget run https://raw.githubusercontent.com/baconzilla123/autist/HEAD/installer.lua")
 print(" ")
 print("Updated! \n")
